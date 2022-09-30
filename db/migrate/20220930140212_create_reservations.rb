2cce00ae86de410ad00cb97ede0db949dbd23809
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-30 18:40:32 +0430

    add references for user

--- db/migrate/20220930140212_create_reservations.rb
+++ db/migrate/20220930140212_create_reservations.rb
@@ -4,6 +4,8 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.string :date
       t.string :city
       t.references :room, null: false, foreign_key: true
+      # the reservations references user foreign key is the react_user_id from the user table
+      t.references :users, foreign_key: { to_table: :users, column: :react_user_id }
 
       t.timestamps
     end
