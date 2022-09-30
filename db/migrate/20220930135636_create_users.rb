bc824c794f34906c980270600d36ef142329db12
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-30 23:10:16 +0300

    Fix tables and associations

--- db/migrate/20220930135636_create_users.rb
+++ db/migrate/20220930135636_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :react_user_id
-      
+
       t.timestamps
     end
   end
--- db/migrate/20220930140212_create_reservations.rb
+++ db/migrate/20220930140212_create_reservations.rb
@@ -4,7 +4,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.string :date
       t.string :city
       t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: { to_table: :users, column: :react_user_id }
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
