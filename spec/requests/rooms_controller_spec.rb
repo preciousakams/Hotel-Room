994035608181638f39a0aeed24bcfdb3f169bc07
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-10-01 21:37:52 +0300

    Add request spec for room post and delete methods

--- spec/requests/rooms_controller_spec.rb
+++ spec/requests/rooms_controller_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Rooms Page', type: :request do
-  describe 'GET #index' do
-    before do
-      @room = Room.create(name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BJVEB/bjveb-deluxe-0003-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px')
+  describe 'POST /Room' do
+    it 'create new room' do
+      post '/api/v1/rooms', params: { room: { name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'a' } }
+      expect(response).to have_http_status(:created)
     end
-    it 'is a HTTP success' do
-      get(api_v1_rooms_path)
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'shows the name in the show page ' do
-      get(api_v1_rooms_path(@room))
-      expect(response.body).to include(@room.name)
+  end
+  describe 'DELETE /Room' do
+    it 'deletes a room' do
+      room = Room.create(name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BJVEB/bjveb-deluxe-0003-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px')
+      delete "/api/v1/rooms/#{room.id}"
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
