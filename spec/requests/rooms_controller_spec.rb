87dae7b7d57b696e618b0c8abe677832059b16a2
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-03 14:05:56 +0430

    check the response status for requesting a specific romm

--- spec/requests/rooms_controller_spec.rb
+++ spec/requests/rooms_controller_spec.rb
@@ -20,6 +20,15 @@ RSpec.describe 'Rooms Page', type: :request do
       expect(JSON.parse(response.body).first.values).to be_an_instance_of(Array)
     end
   end
+  describe 'GET /Room' do
+    it 'returns a response with the status code 200' do
+      room = Room.create(name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BJVEB/bjveb-deluxe-0003-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px')
+      get "/api/v1/rooms/#{room.id}"
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+
   describe 'POST /Room' do
     it 'create new room' do
       post '/api/v1/rooms', params: { room: { name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'a' } }
