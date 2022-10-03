37cde757e01681226bc290af960a4d1263b27cda
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-03 13:41:20 +0430

    solved the validation issue for room class model

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,5 +4,5 @@ class Room < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :size, presence: true, numericality: { only_integer: true }
-  validates :view, :bedding, :image, :presence: true
+  validates :view, :bedding, :image, presence: true
 end
