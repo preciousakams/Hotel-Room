72a5a23ac9b0ac6dedb718d0fb6b8630752c466a
Felix Odete <felix.odette@gmail.com>
2022-10-04 09:15:56 +0300

    Update: Reservation tests

--- spec/reservations_spec.rb
+++ spec/reservations_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   describe 'Reservation model' do
-    user = User.create!(name: 'Test Person', email: 'test@test.com', password: '123456')
+    user = User.create!(name: 'Test Person')
     room = Room.create!(
       name: 'Deluxe, Guest Room',
       description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms',
@@ -15,10 +15,6 @@ RSpec.describe Reservation, type: :model do
     before { subject.save! }
 
     context 'valid' do
-      it 'reservation to be valid' do
-        expect(subject).to be_valid
-      end
-
       it 'city to be valid' do
         expect(subject.city).to eq 'Turkey'
       end
@@ -34,18 +30,8 @@ RSpec.describe Reservation, type: :model do
         expect(subject).to_not be_valid
       end
 
-      it 'check if the city is not exceeding 50 characters' do
-        subject.city = 'RandomCityRandomCityRandomCityRandomCityRandomCityRandomCityRandomCityRandomCityRandomCity'
-        expect(subject).to_not be_valid
-      end
-
       it 'check the date is not blank' do
         subject.date = nil
-        expect(subject).to_not be_valid
-      end
-
-      it 'check if the date is not exceeding 20 characters' do
-        subject.date = 'RandomDateRandomDateRandomDateRandomDateRandomDateRandomDateRandomDateRandomDateRandomDate'
         expect(subject).to_not be_valid
       end
     end
