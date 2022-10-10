a0e966d54890629ccd805e0ca29c33a3fc51f709
Felix Odete <felix.odette@gmail.com>
2022-10-10 18:41:55 +0300

    Create: Reservation test

--- spec/model/reservations_spec.rb
+++ spec/model/reservations_spec.rb
@@ -1,39 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  describe 'Reservation model' do
-    user = User.create!(name: 'Test Person')
-    room = Room.create!(
-      name: 'Deluxe, Guest Room',
-      description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms',
-      size: '45',
-      view: 'Sea',
-      bedding: 'twin',
-      image: 'a'
-    )
-    subject { Reservation.new(user_id: user.id, room_id: room.id, city: 'Turkey', date: '03-10-2022') }
-    before { subject.save! }
+  before :all do
+    @user = User.create(email: 'test@test.com', name: 'Test', username: 'test')
+    @room = Room.create(name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BJVEB/bjveb-deluxe-0003-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px')
+    @reservation = Reservation.new(user_id: @user.id, car_id: @room.id, date: '2022-10-11', city: 'test')
+  end
 
-    context 'valid' do
-      it 'city to be valid' do
-        expect(subject.city).to eq 'Turkey'
-      end
+  it 'is valid' do
+    expect(@reservation).to be_valid
+  end
 
-      it 'date to be valid' do
-        expect(subject.date).to eq '03-10-2022'
-      end
-    end
+  it 'date should be present' do
+    @reservation.date = nil
+    expect(@reservation).to_not be_valid
+  end
 
-    context 'invalid' do
-      it 'check the city is not blank' do
-        subject.city = nil
-        expect(subject).to_not be_valid
-      end
-
-      it 'check the date is not blank' do
-        subject.date = nil
-        expect(subject).to_not be_valid
-      end
-    end
+  it 'city should be present' do
+    @reservation.city = nil
+    expect(@reservation).to_not be_valid
   end
 end
