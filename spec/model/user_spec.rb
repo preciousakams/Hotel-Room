6eb64ed5956b7b8e2c86a62ffd5c3bf596160261
Precious Akams <preciousakams@yahoo.com>
2022-10-11 12:05:37 +0100

    user model test

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      user = User.new(name: 'John')
+      expect(user).to be_valid
+    end
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+  end
+end
