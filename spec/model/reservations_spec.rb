9a00592fd08884a030ef9fc0f091cb5d6855cffc
Felix Odete <felix.odette@gmail.com>
2022-10-11 10:15:48 +0300

    Update: Test

--- spec/model/reservations_spec.rb
+++ spec/model/reservations_spec.rb
@@ -2,19 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   before :all do
-    @user = User.new(email: 'test@test.com', name: 'Johnny', username: 'Test')
+    @user = User.new(name: 'Johnny')
     @room = Room.new(name: 'Deluxe, Guest room', description: 'Deluxe, Guest room, 1 King. Sleep soundly in Bodrum on the indulgent bedding and crisp linens in all of our resort hotel rooms', size: '45', view: 'Sea', bedding: 'Twin', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BJVEB/bjveb-deluxe-0003-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px')
-    @reservation = Reservation.new(user_id: @user.user_id, room_id: @room.room_id, start_date: '03-10-2022', end_date: '04-11-2022', city: 'Nairobi')
-  end
-
-  context 'valid' do
-    it 'city to be valid' do
-      subject.city.to eq 'Nairobi'
-    end
-
-    it 'date to be valid' do
-      expect(subject.date).to eq '03-10-2022'
-    end
+    @reservation = Reservation.new(user_id: @user.name, room_id: @room.name, date: '03-10-2022', city: 'Nairobi')
   end
 
   context 'invalid' do
@@ -26,6 +16,18 @@ RSpec.describe Reservation, type: :model do
     it 'check the date is not blank' do
       subject.date = nil
       expect(subject).to_not be_valid
+    end
+  end
+
+  context 'valid' do
+    it 'check the city is valid' do
+      expect(@reservation.city).to eq 'Nairobi'
+    end
+    it 'check if the date is valid' do
+      expect(@reservation.date).to eq '03-10-2022'
+    end
+    it 'check if name is valid' do
+      expect(@user.name).to eq 'Johnny'
     end
   end
 end
