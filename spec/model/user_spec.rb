23349d9f354927f8a967d948cf993e0397b99f1d
kamzzy <preciousakams@yahoo.com>
2022-10-11 12:07:42 +0100

    associations tested

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe User, type: :model do
       user = User.new(name: nil)
       expect(user).to_not be_valid
     end
+    describe 'Associations' do
+      it 'has many reservations' do
+        assc = described_class.reflect_on_association(:reservations)
+        expect(assc.macro).to eq :has_many
+      end
+    end
   end
 end
